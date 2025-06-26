4758ebb4a2bc45256a61bef8972856e42dcaf939
MaartenS11 <maarten.steevens@gmail.com>
2025-06-26 10:50:19 +0200

    Make the console use the default monospaced font

--- src/main/kotlin/be/ugent/topl/mio/ui/ConsoleWindow.kt
+++ src/main/kotlin/be/ugent/topl/mio/ui/ConsoleWindow.kt
@@ -2,10 +2,12 @@ package be.ugent.topl.mio.ui
 
 import be.ugent.topl.mio.debugger.Debugger
 import java.awt.Dimension
+import java.awt.Font
 import javax.swing.JFrame
 import javax.swing.JScrollPane
 import javax.swing.JTextArea
 import javax.swing.text.DefaultCaret
+import javax.swing.text.StyleContext
 
 class ConsoleWindow(debugger: Debugger) : JFrame("Console") {
     init {
@@ -13,6 +15,8 @@ class ConsoleWindow(debugger: Debugger) : JFrame("Console") {
         val textArea = JTextArea().apply {
             isEditable = false
         }
+        val st = StyleContext.getDefaultStyleContext()
+        textArea.font = st.getFont(Font.MONOSPACED, Font.PLAIN, 13)
         val scrollPane = JScrollPane(textArea)
         val caret = textArea.caret as DefaultCaret
         caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE)
